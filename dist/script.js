90594d8a9370999544af3dea8a9f30076e156e8e
Olga Kulczycka <olga@webjaksklep.eu>
2025-09-30 21:47:16 +0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+console.log('Hello!');
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
